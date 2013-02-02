609bd966fa4e6416a30a08429addf84ac5a905a7
Arthur Britto <ahbritto@gmail.com>
2013-02-02 02:47:34 -0800

    Add gateways to config.

--- config-example.js
+++ config-example.js
@@ -1,3 +1,10 @@
+var irc_config = {
+  enable: false,
+};
+
+var gateways = {
+};
+
 var remote_config = {
   'trusted' : true,
   'websocket_ip' : "127.0.0.1",
@@ -7,11 +14,8 @@ var remote_config = {
   'local_fee' : true,
 };
 
-var irc_config = {
-  enable: false,
-};
-
 exports.irc_config	= irc_config;
 exports.remote_config	= remote_config;
+exports.gateways	= gateways;
 
 // vim:sw=2:sts=2:ts=8:et
