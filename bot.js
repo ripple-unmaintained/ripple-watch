cafbd3f35f0fd28ae72cc12e8d9aca1a11c696b0
Arthur Britto <ahbritto@gmail.com>
2013-02-02 02:48:11 -0800

    Output offer takes from OfferCreate.

--- bot.js
+++ bot.js
@@ -3,7 +3,10 @@
 var Amount        = require("ripple-lib").Amount;
 var Currency      = require("ripple-lib").Currency;
 var Remote        = require("ripple-lib").Remote;
+var UInt160       = require("ripple-lib").UInt160;
+var extend	  = require("extend");
 var irc	          = require("irc");
+var gateways      = require("./config").gateways;
 var irc_config    = require("./config").irc_config;
 var remote_config = require("./config").remote_config;
 
@@ -36,7 +39,7 @@ client
 var writeMarket = function (message) {
   if (message)
   {
-    console.log(message);
+    console.log("M: " + message);
 
     if (self.irc) {
       client.say("#ripple-market", message);
@@ -47,7 +50,7 @@ var writeMarket = function (message) {
 var writeWatch = function (message) {
   if (message)
   {
-    console.log(message);
+    console.log("W: " + message);
 
     if (self.irc) {
       client.say("#ripple-watch", message);
@@ -76,30 +79,22 @@ var remote  =
               if (self.totalCoins !== lh.ledger.totalCoins) {
                 self.totalCoins = lh.ledger.totalCoins;
 
-                self.totalCoins.match(/^(.*)(......)$/);
-
-                var matches = lh.ledger.totalCoins.match(/^(.*)(......)$/);
-                var xrp_whole   = matches[1];
-                var xrp_fraction  = matches[2];
-
                 // console.log("ledger_header: ", JSON.stringify(lh));
 
-                writeWatch("Ledger #" + m.ledger_index + " Total XRP: " + xrp_whole + "." + xrp_fraction);
+                writeWatch("Ledger #" + m.ledger_index + " Total XRP: " + Amount.from_json(self.totalCoins).to_human());
               }
             })
           .request()
       })
     .on('transaction', function (m) {
-        var say;
+        var say_watch;
 
         if (m.transaction.TransactionType === 'Payment')
         {
           var amount    = Amount.from_json(m.transaction.Amount);
           var currency  = amount.currency();
 
-          say = amount.to_human()
-                  + " "
-                  + currency.to_human()
+          say_watch = amount.to_human_full({ gateways: gateways })
                   + " "
                   + m.transaction.Account + " > " + m.transaction.Destination;
         }
@@ -107,15 +102,15 @@ var remote  =
         {
           console.log("transaction: ", JSON.stringify(m, undefined, 2));
 
-          say = m.transaction.Account;
+          say_watch = m.transaction.Account;
         }
         else if (m.transaction.TransactionType === 'TrustSet')
         {
           var amount    = Amount.from_json(m.transaction.LimitAmount);
           var currency  = amount.currency();
 
-          say = amount.to_human()
-                  + " "
+          say_watch = amount.to_human()
+                  + "/"
                   + currency.to_human()
                   + " "
                   + m.transaction.Account + " > " + m.transaction.LimitAmount.issuer;
@@ -123,30 +118,73 @@ var remote  =
         else if (m.transaction.TransactionType === 'OfferCreate')
         {
           console.log("transaction: ", JSON.stringify(m, undefined, 2));
-//     "TakerGets": {
-//       "currency": "AUD",
-//       "issuer": "rBcYpuDT1aXNo4jnqczWJTytKGdBGufsre",
-//       "value": "0.1"
-//     },
-//     "TakerPays": "2000000000",
-//     "TransactionType": "OfferCreate",
-          say = m.transaction.Account;
+
+          say_watch = m.transaction.Account
+                + " offers " + Amount.from_json(m.transaction.TakerGets).to_human_full({ gateways: gateways })
+                + " for " + Amount.from_json(m.transaction.TakerPays).to_human_full({ gateways: gateways });
+
+          if (m.engine_result === 'tesSUCCESS')
+          {
+            m.meta.AffectedNodes.forEach(function (n) {
+                if ('ModifiedNode' in n && n.ModifiedNode.LedgerEntryType === 'Offer') {
+                  var pf  = n.ModifiedNode.PreviousFields;
+                  var ff  = n.ModifiedNode.FinalFields;
+
+                  var taker_got   = Amount.from_json(pf.TakerGets).subtract(Amount.from_json(ff.TakerGets));
+                  var taker_paid  = Amount.from_json(pf.TakerPays).subtract(Amount.from_json(ff.TakerPays));
+
+                  if (taker_got.is_native())
+                  {
+                    [taker_got, taker_paid] = [taker_paid, taker_got];
+                  }
+
+                  if (taker_paid.is_native())
+                  {
+                    var gateway = gateways[taker_got.issuer().to_json()];
+
+                    if (gateway)
+                    {
+                      writeMarket(
+                          gateway
+                          + " " + taker_paid.to_human()
+                          + " @ " + taker_got.multiply(Amount.from_json("1000000")).divide(taker_paid).to_human()
+                          + " " + taker_got.currency().to_human()
+                        );
+                    }
+                    else
+                    {
+                      // Ignore non-reknown issuer.
+                    }
+//                    writeMarket(
+//                      taker_paid.to_human_full({ gateways: gateways })
+//                      + " for "
+//                      + taker_got.to_human_full({ gateways: gateways })
+//                      );
+                  }
+                  else
+                  {
+                    // Ignore IOU for IOU.
+console.log("*: ignore");
+                  }
+                }
+              });
+          }
         }
         else if (m.transaction.TransactionType === 'OfferCancel')
         {
           console.log("transaction: ", JSON.stringify(m, undefined, 2));
 
-          say = m.transaction.Account;
+          say_watch = m.transaction.Account;
         }
 
-        if (say)
+        if (say_watch)
         {
           var output  =
               (m.engine_result === 'tesSUCCESS'
                 ? ""
                 : m.engine_result + ": ")
               + m.transaction.TransactionType + " "
-              + say;
+              + say_watch;
 
           writeWatch(output);
         }
