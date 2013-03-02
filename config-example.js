6a10747aa152f55bdfaa4300f7eb8e00b8f8a2d0
Arthur Britto <ahbritto@gmail.com>
2013-03-02 10:12:11 -0800

    Improve config example.

--- config-example.js
+++ config-example.js
@@ -3,6 +3,15 @@ var irc_config = {
 };
 
 var gateways = {
+//  rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B:  'bitstamp',
+//  rBcYpuDT1aXNo4jnqczWJTytKGdBGufsre: 'weex', // AUD
+//  rpvfJ4mR6QQAeogpXEKnuyGBx8mYCSnYZi: 'weex', // BTC
+//  r47RkFi1Ew3LvCNKT6ufw3ZCyj5AJiLHi9: 'weex', // CAD
+//  r9vbV3EHvXWjSkeQ6CAcYVPGeq7TuiXY2X: 'weex', // USD
+};
+
+var hotwallets = {
+//  rrpNnNLKrartuEqfJGpqyDwPj1AFPg9vn1: 'bitstamp',
 };
 
 var remote_config = {
